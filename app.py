3fcd8b96cc9caa7ad907eaa1cec1b04b15d10bfc
darko <ki14.skakun@gmail.com>
2023-10-10 15:27:52 +0300

    update requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from src import create_app, db
+from src.models.models import Role
 
 app = create_app()
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        Role.insert_roles()
     app.run(host="0.0.0.0", debug=True)
